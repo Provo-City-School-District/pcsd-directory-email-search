5e12979afb71ea505b32b0d510faff01c57db5e1
Josh Espinoza <joshespi@gmail.com>
2025-07-09 14:52:34 -0600

    fix SQL injection vulnerability

--- email-search-plugin.php
+++ email-search-plugin.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name: PCSD Directory Email Search
  * Description: A plugin to search for emails in our postmeta where our website directory information is stored.
@@ -9,12 +10,14 @@
 // Add a new page to the WordPress admin dashboard.
 add_action('admin_menu', 'email_search_plugin_menu');
 
-function email_search_plugin_menu() {
+function email_search_plugin_menu()
+{
     add_menu_page('Directory Email Search Plugin Page', 'Dir Email Search', 'manage_options', 'email-search-plugin', 'email_search_plugin_page', 'https://globalassets.provo.edu/image/icons/pcsd-icon-16x16.png', 6);
 }
 
 // The function that outputs the HTML for the admin page.
-function email_search_plugin_page() {
+function email_search_plugin_page()
+{
     echo '<h1>Directory Email Search</h1>';
     echo '<form method="post" action="">';
     echo '<p><label for="email">Email: </label><input type="text" name="email" id="email"></p>';
@@ -24,14 +27,21 @@ function email_search_plugin_page() {
     if (isset($_POST['email'])) {
         global $wpdb;
         $email = sanitize_text_field($_POST['email']);
-        
+
         //Query search
-        $results = $wpdb->get_results("SELECT post_id, meta_key, meta_value FROM {$wpdb->prefix}postmeta WHERE meta_key = 'email' AND meta_value LIKE '%$email%'", ARRAY_A);
+        $results = $wpdb->get_results(
+            $wpdb->prepare(
+                "SELECT post_id, meta_key, meta_value FROM {$wpdb->prefix}postmeta WHERE meta_key = %s AND meta_value LIKE %s",
+                'email',
+                '%' . $wpdb->esc_like($email) . '%'
+            ),
+            ARRAY_A
+        );
         //display results
         echo '<ul>';
         foreach ($results as $result) {
             echo '<li>';
-            echo $result['meta_value'] . ' found in post <a href="https://provo.edu/wp-admin/post.php?post='.$result['post_id'] .'&action=edit&classic-editor">' . $result['post_id'] . '</a>';
+            echo $result['meta_value'] . ' found in post <a href="https://provo.edu/wp-admin/post.php?post=' . $result['post_id'] . '&action=edit&classic-editor">' . $result['post_id'] . '</a>';
             echo '</li>';
         }
         echo '</ul>';
@@ -41,6 +51,4 @@ function email_search_plugin_page() {
             echo '<p>No results found.</p>';
         }
     }
-
 }
-?>
